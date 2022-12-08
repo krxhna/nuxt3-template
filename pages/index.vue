6821d5e2ce9274423c7c21d47eda075ae8798008
Krish <34429668+krxhna@users.noreply.github.com>
2022-12-08 18:40:25 +0800

    preetier ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-    <h1 class="btn">meoww</h1>
+    <div class="flex justify-center bg-black  items-center h-screen">
+<div class="p-3 bg-gradient-to-tr from-green-500 to-blue-500 shadow-2xl shadow-green-700 rounded-lg w-96 h-96 ">
+
+<div class="text-white w-full h-full bg-black rounded-lg p-3 text-3xl capitalize gap-3 flex flex-col justify-center items-center text-right">
+    <h3>
+    nuxt3
+    </h3>
+    <h3>
+    iconify
+    </h3>
+    <h3>
+    tailwind
+    </h3>
+    <h3>
+    prisma
+    </h3>
+    
+</div>
+</div>
 
     </div>
 </template>
